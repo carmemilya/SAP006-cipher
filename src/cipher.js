f7bb1e3f919ce29194f24bc3c828d9f46b4cc79f
Carmen Emilia <carmemilya1@gmail.com>
2021-06-08 12:59:45 -0300

    Criação de CSS e HTML do projeto.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,20 @@
+
+/* A  B  C  D  E  F  G  H  I   J   K   L   M   N   O   P   Q
+   0  1  2  3  4  5  6  7  8  9   10  11  12  13  14  15  16  
+
+   R   S   T   U   V   X   W   Y   Z 
+   17  18  19  20  21  22  23  24  25    
+*/
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+"S","T","U","V","X","W","Y","Z"]; //numero de vetores: 26
+
+
 const cipher = {
-  // ...
+
+  
 };
 
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import cipher from './cipher.js';
+const name= prompt("Como você se chama?");
+document.getElementById("root").innerHTML = ("Olá, " + name + "!");
 
-console.log(cipher);
+
+function codificar(){
+    const offset = document.getElementById("offsetNumber").value
+    const text = document.getElementById("text").value;
+    const arr = text.split("");
+
+    for (i = 0; i< arr.length; i++){
+
+        //falta terminar essa função
+
+    }
+}
+
+import cipher from './cipher.js';
+// não estou conseguindo importar o código no cipher para o o index.js/html
